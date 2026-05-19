c57a550ab44f3d06cbc13fc62d4c4a9a81ee79fe
prinses-racwa <rajwaamelia@gmail.com>
2026-05-19 19:05:02 +0700

    menambahkan class RekeningSyariah public ReekeningBank

--- tugas.cpp
+++ tugas.cpp
@@ -5,3 +5,21 @@ class RekeningBank {
 public:
     virtual void potongAdmin() = 0;
 };
+
+class RekeningSyariah : public RekeningBank {
+private:
+    string noRek;
+    double saldo;
+
+public:
+    RekeningSyariah(string no, double saldoAwal) {
+        noRek = no;
+        saldo = saldoAwal;
+    }
+
+    void potongAdmin() {
+        cout << "Rekening Syariah: Tidak ada potongan admin bulanan." << endl;
+        cout << "No Rek: " << noRek << " | Saldo Akhir: Rp " << saldo << endl;
+        cout << "----------------------------------------------------" << endl;
+    }
+};
